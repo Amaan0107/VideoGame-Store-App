72ab1a0585dc8c8c6ef70c97c3bc032a31867d93
Amaan0107 <Adilawar2007@gmail.com>
2025-12-15 12:34:46 -0800

    Starting code in ProfileController

--- src/main/java/org/yearup/controllers/ProfileController.java
+++ src/main/java/org/yearup/controllers/ProfileController.java
@@ -1,4 +1,27 @@
 package org.yearup.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.yearup.data.ProfileDao;
+import org.yearup.data.UserDao;
+import org.yearup.models.Profile;
+import org.yearup.models.User;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("profile")
+@CrossOrigin
 public class ProfileController {
+    private final ProfileDao profileDao;
+    private final UserDao userDao;
+
+    @Autowired
+    public ProfileController(ProfileDao profileDao, UserDao userDao) {
+        this.profileDao = profileDao;
+        this.userDao = userDao;
+    }
 }
